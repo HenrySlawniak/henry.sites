c7130a85f5cb3ac1a44c271536c4892ffb6295af
Henry Slawniak <meggawatts@gmail.com>
2017-08-05 05:58:56 -0500

    More crosscompiling

--- crosscompile.sh
+++ crosscompile.sh
@@ -4,13 +4,16 @@ mkdir -p bin/
 
 go get
 
-for target in windows:amd64 linux:amd64 darwin:amd64 linux:386 linux:arm; do
-  echo "Compiling $target"
-  export GOOS=$(echo $target | cut -d: -f1) GOARCH=$(echo $target | cut -d: -f2)
-  OUT=bin/$(basename $(echo $PWD))_${GOOS}_${GOARCH}
-  if [ $GOOS == "windows" ]
-  then
-    OUT="$OUT.exe"
-  fi
-  bash -c "go build -ldflags '-w -X main.buildTime=$(date +'%b-%d-%Y-%H:%M:%S') -X main.commit=$(git describe --always --dirty=*)' -v -o $OUT ."
+for os in windows linux darwin plan9 freebsd dragonfly netbsd solaris; do
+  for arch in amd64 386 arm arm64 ppc64 ppc64le mips mipsle mips64 mips64le; do
+    target="$os:$arch"
+    echo "Compiling $target"
+    export GOOS=$(echo $target | cut -d: -f1) GOARCH=$(echo $target | cut -d: -f2)
+    OUT=bin/$(basename $(echo $PWD))_${GOOS}_${GOARCH}
+    if [ $GOOS == "windows" ]
+    then
+      OUT="$OUT.exe"
+    fi
+    bash -c "go build -ldflags '-w -X main.buildTime=$(date +'%b-%d-%Y-%H:%M:%S') -X main.commit=$(git describe --always --dirty=*)' -v -o $OUT ."
+  done
 done
