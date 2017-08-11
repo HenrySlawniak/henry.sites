3190c98800e5c3f6c5ae621cb8f6219d364566ba
Henry Slawniak <meggawatts@gmail.com>
2017-08-10 21:28:52 -0500

    Better building

--- systemd.sh
+++ systemd.sh
@@ -8,4 +8,6 @@ go build -v
 
 -c "go build -ldflags '-w -X main.buildTime=$(date +'%b-%d-%Y-%H:%M:%S') -X main.commit=$(git describe --always --dirty=*)' -v ."
 
+chmod a+x /var/henry.sites/henry.sites
+
 authbind --deep /var/henry.sites/henry.sites
