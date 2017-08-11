1da2f711814885336d398c1061e6891123b83ce2
Henry Slawniak <meggawatts@gmail.com>
2017-08-10 21:23:20 -0500

    More sensible systemd stuff

--- systemd.sh
+++ systemd.sh
@@ -6,4 +6,6 @@ go get -v
 
 go build -v
 
+go build -ldflags '-w -X main.buildTime=$(date +'%b-%d-%Y-%H:%M:%S') -X main.commit=$(git describe --always --dirty=*)' -v .
+
 authbind --deep /var/henry.sites/henry.sites
